5b0f759660683006403a2118ae114a0902c7141b
christianresende <christianresende@gmail.com>
2021-11-20 13:48:47 -0300

    alteração footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="bg-gray-700 p-4">
       <div className="container mx-auto text-center text-white font-bold">
         {" "}
-        Projeto desenvolvido por: Christian Resende /{" "}
+        Desenvolvido por: Christian Resende <br />
         <a
           className="hover:underline"
           href="https://www.linkedin.com/in/christian-resende/"
@@ -20,10 +20,6 @@ const Footer = () => {
           {" "}
           GitHub
         </a>{" "}
-        <div className="mt-2">
-          <img className="w-40 inline p-4" src="/logo_semana_fsm.png" />
-          <img className="w-40 inline p-4" src="/logo_devpleno.png" />
-        </div>
       </div>
     </div>
   );
